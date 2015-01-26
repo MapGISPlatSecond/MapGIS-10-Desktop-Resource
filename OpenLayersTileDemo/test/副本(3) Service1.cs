5dcae2e6c6344e41781da7ecf1f5f4cd90479b13
MapGISPlatSecond <zhanghui@mapgis.com>
2015-01-26 17:18:15 +0800

    add a file

---
+++ OpenLayersTileDemo/test/副本(3) Service1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data;
+using System.Configuration;
+using System.Data.OleDb;
+namespace test
+{
+    // Start the service and browse to http://<machine_name>:<port>/Service1/help to view the service's generated help page
+    // NOTE: By default, a new instance of the service is created for each call; change the InstanceContextMode to Single if you want
+    // a single instance of the service to process all calls.	
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    // NOTE: If the service is renamed, remember to update the global.asax.cs file
+    public class Service1
+    {
+        // TODO: Implement the collection resource that will contain the SampleItem instances
+        [WebGet(UriTemplate = "?sql={sql}", ResponseFormat = WebMessageFormat.Json)]
+        public string GetData(string sql)
+        {
+
+            string ConnectionString = ConfigurationManager.ConnectionStrings[1].ToString();
+            // string ConnectionString = ConfigurationManager.AppSettings["PROCConnectionString"];
+            OleDbConnection con = new OleDbConnection(ConnectionString);
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, con);
+            DataSet ds = new DataSet();
+            try
+            {
+                con.Open();
+                adapter.Fill(ds);
+
+            }
+            finally
+            {
+                con.Close();
+            }
+            return ToJson(ds.Tables[0]);
+        }
+
+        [WebInvoke(UriTemplate = "?sql={sql}", Method = "POST")]
+        public void appendDate(string sql)
+        {
+            string ConnectionString = ConfigurationManager.AppSettings["PROCConnectionString"];
+            OleDbConnection con = new OleDbConnection(ConnectionString);
+            try
+            {
+                con.Open();
+                OleDbCommand dbCommand = new OleDbCommand();
+                dbCommand.Connection = con;
+                dbCommand.CommandText = sql;
+                dbCommand.ExecuteNonQuery();
+
+            }
+            finally
+            {
+                con.Close();
+
+            }
+
+        }
+
+        [WebGet(UriTemplate = "")]
+        public List<SampleItem> GetCollection()
+        {
+            // TODO: Replace the current implementation to return a collection of SampleItem instances
+            return new List<SampleItem>() { new SampleItem() { Id = 1, StringValue = "Hello" } };
+        }
+
+        [WebInvoke(UriTemplate = "", Method = "POST")]
+        public SampleItem Create(SampleItem instance)
+        {
+            // TODO: Add the new instance of SampleItem to the collection
+            throw new NotImplementedException();
+        }
+
+        [WebGet(UriTemplate = "{id}")]
+        public SampleItem Get(string id)
+        {
+            // TODO: Return the instance of SampleItem with the given id
+            throw new NotImplementedException();
+        }
+
+        [WebInvoke(UriTemplate = "{id}", Method = "PUT")]
+        public SampleItem Update(string id, SampleItem instance)
+        {
+            // TODO: Update the given instance of SampleItem in the collection
+            throw new NotImplementedException();
+        }
+
+        [WebInvoke(UriTemplate = "{id}", Method = "DELETE")]
+        public void Delete(string id)
+        {
+            // TODO: Remove the instance of SampleItem with the given id from the collection
+            throw new NotImplementedException();
+        }
+
+
+        /// <summary> 
+        /// Datatable转换为Json 
+        /// </summary> 
+        /// <param name="table">Datatable对象</param> 
+        /// <returns>Json字符串</returns> 
+        public static string ToJson(DataTable dt)
+        {
+            StringBuilder jsonString = new StringBuilder();
+
+            if (dt.Rows.Count == 0)
+            {
+                jsonString.Append("[{}]");
+                return jsonString.ToString();
+            }
+
+            jsonString.Append("[");
+            DataRowCollection drc = dt.Rows;
+            for (int i = 0; i < drc.Count; i++)
+            {
+                jsonString.Append("{");
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    string strKey = dt.Columns[j].ColumnName;
+                    string strValue = drc[i][j].ToString();
+                    Type type = dt.Columns[j].DataType;
+                    jsonString.Append("\"" + strKey + "\":");
+                    strValue = StringFormat(strValue, type);
+                    if (j < dt.Columns.Count - 1)
+                    {
+                        jsonString.Append(strValue + ",");
+                    }
+                    else
+                    {
+                        jsonString.Append(strValue);
+                    }
+                }
+                jsonString.Append("},");
+            }
+            jsonString.Remove(jsonString.Length - 1, 1);
+            jsonString.Append("]");
+            return jsonString.ToString();
+        }
+
+        /// <summary> 
+        /// 格式化字符型、日期型、布尔型 
+        /// </summary> 
+        /// <param name="str"></param> 
+        /// <param name="type"></param> 
+        /// <returns></returns> 
+        private static string StringFormat(string str, Type type)
+        {
+            if (type == typeof(string))
+            {
+                str = String2Json(str);
+                str = "\"" + str + "\"";
+            }
+            else if (type == typeof(DateTime))
+            {
+                str = "\"" + Convert.ToDateTime(str).ToShortDateString() + "\"";
+            }
+            else if (type == typeof(bool))
+            {
+                str = str.ToLower();
+            }
+
+            if (str.Length == 0)
+                str = "\"\"";
+
+            return str;
+        }
+
+        /// <summary> 
+        /// 过滤特殊字符 
+        /// </summary> 
+        /// <param name="s"></param> 
+        /// <returns></returns> 
+        private static string String2Json(String s)
+        {
+            System.Text.StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s.ToCharArray()[i];
+
+                switch (c)
+                {
+                    case '\"':
+                        sb.Append("\\\""); break;
+                    case '\\':
+                        sb.Append("\\\\"); break;
+                    case '/':
+                        sb.Append("\\/"); break;
+                    case '\b':
+                        sb.Append("\\b"); break;
+                    case '\f':
+                        sb.Append("\\f"); break;
+                    case '\n':
+                        sb.Append("\\n"); break;
+                    case '\r':
+                        sb.Append("\\r"); break;
+                    case '\t':
+                        sb.Append("\\t"); break;
+                    default:
+                        sb.Append(c); break;
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
